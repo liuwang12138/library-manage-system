02fd14faaa75631f800a527b053a96eb5bbabf31
liuwang <liuwang@ilarge.cn>
2020-04-15 19:02:58 +0800

    scheduled task

--- src/main/java/com/godric/lms/task/UpdateBlacklistTask.java
+++ src/main/java/com/godric/lms/task/UpdateBlacklistTask.java
@@ -16,12 +16,19 @@ public class UpdateBlacklistTask {
     @Autowired
     BlacklistService blacklistService;
 
-//    @Scheduled(cron = "*/5 * * * * ?")
+//    @Scheduled(cron = "5 9 * * *")
 //    public void updateBlacklist() {
 //        blacklistService.scanBreakRuleList();
 //    }
-//    @Scheduled(cron = "*/2 * * * * ?")
+//
+//    @Scheduled(cron = "5 13 * * *")
 //    public void updateBlacklist2() {
+//        blacklistService.scanBreakRuleList();
+//    }
+//
+//    @Scheduled(cron = "5 18 * * *")
+//    public void updateBlacklist3() {
+//        blacklistService.scanBreakRuleList();
 //    }
 
 }
